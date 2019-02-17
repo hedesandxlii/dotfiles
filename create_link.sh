dc09e1536983439ff2f95c6438e202efb4de9546
André <an7821he-s@student.lu.se>
2019-02-17 15:58:58 +0100

    Add force flag for answer-locked link creation

--- create_link.sh
+++ create_link.sh
@@ -3,10 +3,10 @@ ln ./my_vimrc ~/.config/nvim/init.vim &&
 	echo Success! &&
 	exit
 echo
-read -p "Do you want to remove your existing ~/.vimrc and link to this one?" -n 1 -r
+read -p "Do you want to remove your existing ~/.config/nvim/init.vim  and link to this one?" -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-ln ./my_vimrc ~/.config/nvim/init.vim && 
+ln -f ./my_vimrc ~/.config/nvim/init.vim && 
 	echo Success!
 fi 
