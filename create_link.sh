a6b4e83492b54f462d38c002d84b7042c47e3830
André <an7821he-s@student.lu.se>
2019-02-24 17:11:45 +0100

    Blind commit

--- create_link.sh
+++ create_link.sh
@@ -1,12 +1,12 @@
 echo Trying to hard-link $(realpath ./my_vimrc) to ~/.vimrc
-ln ./my_vimrc ~/.config/nvim/init.vim && 
+ln ./my_vimrc ~/.vimrc && 
 	echo Success! &&
 	exit
 echo
-read -p "Do you want to remove your existing ~/.config/nvim/init.vim  and link to this one?" -n 1 -r
+read -p "Do you want to remove your existing ~/.vimrc and (force) link to this one?" -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-ln -f ./my_vimrc ~/.config/nvim/init.vim && 
+ln -f ./my_vimrc ~/.vimrc && 
 	echo Success!
 fi 
