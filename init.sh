9b0ae43dd53a37fb60f5b830f895ae2da9b2f5f4
André Hedesand <andre.hedesand@volvocars.com>
2019-07-24 17:45:41 +0200

    Add i3 config to this repo. This is now gonna be more of a 'dotfiles' repo. init script got a facelift.

--- init.sh
+++ init.sh
@@ -1,12 +1,24 @@
-echo Trying to hard-link $(realpath ./my_vimrc) to ~/.vimrc
-ln ./my_vimrc ~/.vimrc && 
-	echo Success! &&
-	exit
-echo
-read -p "Do you want to remove your existing ~/.vimrc and (force) link to this one?" -n 1 -r
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-ln -f ./my_vimrc ~/.vimrc && 
-	echo Success!
-fi 
+RELATIVE_PATH_VIMRC=./vimrc/my_vimrc
+RELATIVE_PATH_I3=./i3
+RELATIVE_PATH_I3STATUS=./i3status
+
+# Creates a link from source to destination
+# $1 source
+# $2 destination
+create_link() {
+    if ln -s $1 $2; then
+        echo "Link success: $1 to $2"
+    else
+        echo "Link failiure $1 to $2"
+        read -p "Do you want to remove $2 and try linking again?" -n 1 -r
+        echo   
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            rm $2
+            ln $1 $2
+        fi
+    fi
+}
+
+create_link $RELATIVE_PATH_VIMRC ~/.vimrc && echo Success!
+create_link $RELATIVE_PATH_I3 ~/.config/i3
+create_link $RELATIVE_PATH_I3STATUS ~/.config/i3status
