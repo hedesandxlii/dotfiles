e6dee87b702048e1e76d2a9f17b24a2e6d8ccf0a
André Hedesand <andre.hedesand@volvocars.com>
2019-07-25 12:32:26 +0200

    Now bashrc is part of this aswell. init now accepts flags -novim, -noi3 and -nobash

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 RELATIVE_PATH_VIMRC=./vimrc/my_vimrc
 RELATIVE_PATH_I3=./i3
 RELATIVE_PATH_I3STATUS=./i3status
+RELATIVE_PATH_BASHRC=./bashrc/my_bashrc
 
 # Creates a link from source to destination
 # $1 source
@@ -14,11 +15,36 @@ create_link() {
         echo   
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             rm $2
-            ln $1 $2
+            create_link $1 $2
         fi
     fi
 }
 
-create_link $RELATIVE_PATH_VIMRC ~/.vimrc && echo Success!
-create_link $RELATIVE_PATH_I3 ~/.config/i3
-create_link $RELATIVE_PATH_I3STATUS ~/.config/i3status
+LINKS=(
+"create_link $RELATIVE_PATH_VIMRC $HOME/.vimrc"
+"create_link $RELATIVE_PATH_I3 $HOME/.config/i3"
+"create_link $RELATIVE_PATH_I3STATUS $HOME/.config/i3status"
+"create_link $RELATIVE_PATH_BASHRC $HOME/.bashrc"
+)
+
+# Remove installs according to options
+for i in "$@"; do
+    case $i in
+        -novim)
+            unset LINKS[0]
+            ;;
+        -noi3)
+            unset LINKS[1]
+            unset LINKS[2]
+            ;;
+        -nobash)
+            unset LINKS[3]
+            ;;
+    esac
+done
+
+# Execute whatever is left in LINKS
+for i in "${!LINKS[@]}"; do
+    ${LINKS[$i]}
+done
+
